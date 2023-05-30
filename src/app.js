983c84657934679dfee20715e72a0669b7fb45d3
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-05-30 08:04:50 +0000

    Commit final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+
+  var adj = ["great", "big"];
+
+  var noun = ["jogger", "racoon", "dicom", "cyborg", "radio", "planet"];
+
+  var ext = [".com", ".net", ".us", ".org", ".io"];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < ext.length; e++) {
+          // ------EXTENSION SIN PUNTO------
+
+          let EditExt = ext[e].substring(1, ext[e].length);
+
+          // ------NOUN RECORTADO--------
+
+          let EditNoun = noun[n].substring(
+            noun[n].length - ext[e].length + 1,
+            noun[n].length
+          );
+
+          // -------CONDICIONAL----------
+
+          if (EditExt === EditNoun) {
+            console.log(
+              "WHAAATT??!! --->  " +
+                noun[n].substring(0, noun[n].length - ext[e].length + 1) +
+                ext[e]
+            );
+          } else console.log(pronoun[p] + adj[a] + noun[n] + ext[e]);
+        }
+      }
+    }
+  }
 };
